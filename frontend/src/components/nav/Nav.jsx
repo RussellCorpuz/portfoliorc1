9296132ac38a62eceee5afbd809c9db2c68385a3
RussellCorpuz <russelljames.fr@icloud.com>
2023-02-02 11:38:14 +0100

    like button

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -5,6 +5,7 @@ import {FaUserTie} from 'react-icons/fa'
 import {MdOutlineWork} from 'react-icons/md'
 import {RiServiceFill} from 'react-icons/ri'
 import {BsTelephoneInboundFill} from 'react-icons/bs'
+import {FcLike} from 'react-icons/fc'
 import {useState} from 'react'
 
 const Nav = () => {
@@ -16,6 +17,7 @@ const Nav = () => {
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWork /></a>
       <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsTelephoneInboundFill /></a>
+      <a href="" onClick={() => setActiveNav('')} className={activeNav === '' ? 'active' : ''}><FcLike /></a>
     </nav>
   )
 }
