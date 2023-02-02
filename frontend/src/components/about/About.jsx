d6c5b4150e6ceac3db65a9dbdd3413371f7f4db3
RussellCorpuz <russelljames.fr@icloud.com>
2023-02-02 02:40:59 +0100

    about

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/mee.jpg'
+import {FaAward} from 'react-icons/fa'
+import {HiUsers} from 'react-icons/hi'
+import {RiFolderOpenFill} from 'react-icons/ri'
 
 
 const About = () => {
   return (
-    <div>About</div>
+      <section id='about'>
+        <h5 className="">Get to know</h5>
+        <h2 className="">About ME</h2>
+
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__me-image">
+              <img src={ME} alt="About Image" className="" />
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+              <article className='about__card'>
+                <FaAward className='about__icon'/>
+                <h5>Experience</h5>
+                <small>Years of Working Experience</small>
+              </article>
+              <article className='about__card'>
+                <HiUsers className='about__icon'/>
+                <h5>Clients</h5>
+                <small>List of Future Clients</small>
+              </article>
+              <article className='about__card'>
+                <RiFolderOpenFill className='about__icon'/>
+                <h5>Projects</h5>
+                <small>List of Projects</small>
+              </article>
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, et. Harum, obcaecati eveniet corporis doloribus aspernatur consectetur totam at ratione facilis deleniti corrupti voluptas cumque praesentium quas velit voluptates hic!</p>
+
+            <a href="#contact" className='btn btn-primary'>Book ME</a>
+          </div>
+
+        </div>
+      </section>
   )
 }
 
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {MdHomeWork} from 'react-icons/md'
+import {FaUserTie} from 'react-icons/fa'
+import {MdOutlineWork} from 'react-icons/md'
+import {RiServiceFill} from 'react-icons/ri'
+import {BsTelephoneInboundFill} from 'react-icons/bs'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav className="">
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><MdHomeWork /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserTie /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdOutlineWork /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsTelephoneInboundFill /></a>
+    </nav>
   )
 }
 
